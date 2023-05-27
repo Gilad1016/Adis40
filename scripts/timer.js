c90aabc731851971580c98c18dfdd344b59dd77f
giladomesi <gilad1016@gmail.com>
2023-05-27 09:38:04 +0300

    init for Yiftach

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 20, 2023 20:00:00").getTime();
+var countDownDate = new Date("May 29, 2023 21:00:00").getTime();
 
 // Update the countdown every 1 second
 var countdown = setInterval(function() {
